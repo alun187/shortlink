4b2dff9157a7ada32c22d8e9f0502b33e1f2b722
alun187 <482911313@qq.com>
2023-11-11 23:46:01 +0800

    自定义异常

---
+++ admin/src/main/java/org/cuidl/shortlink/admin/common/convention/exception/ServiceException.java
@@ -0,0 +1,36 @@
+package org.cuidl.shortlink.admin.common.convention.exception;
+
+import java.util.Optional;
+import org.cuidl.shortlink.admin.common.convention.errorcode.BaseErrorCode;
+import org.cuidl.shortlink.admin.common.convention.errorcode.IErrorCode;
+
+
+/**
+ * 服务端异常
+ */
+public class ServiceException extends AbstractException {
+
+    public ServiceException(String message) {
+        this(message, null, BaseErrorCode.SERVICE_ERROR);
+    }
+
+    public ServiceException(IErrorCode errorCode) {
+        this(null, errorCode);
+    }
+
+    public ServiceException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public ServiceException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(Optional.ofNullable(message).orElse(errorCode.message()), throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
