b2eee0123f66f1538a2a01f6b047265904c36a53
alun187 <482911313@qq.com>
2024-02-20 17:13:47 +0800

    修改用户登录验证拦截器

--- admin/src/main/java/org/cuidl/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/cuidl/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,5 +1,6 @@
 package org.cuidl.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
 import jakarta.servlet.Filter;
@@ -8,11 +9,18 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.cuidl.shortlink.admin.common.convention.exception.ClientException;
+import org.cuidl.shortlink.admin.common.convention.result.Results;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
+
+import static org.cuidl.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -23,26 +31,57 @@ public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate redisTemplate;
 
     private static final List<String> IGNORE_URL = Lists.newArrayList(
-            "/api/shortlink/v1/user/login",
-            "/api/shortlink/v1/user",
-            "/api/shortlink/v1/user/has-username");
+            "/api/shortlink/admin/v1/user/login",
+            "/api/shortlink/admin/v1/user/has-username");
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String requestURI = httpServletRequest.getRequestURI();
         if (!IGNORE_URL.contains(requestURI)) {
-            String username = httpServletRequest.getHeader("username");
-            String token = httpServletRequest.getHeader("token");
-            Object userInfoJsonStr = redisTemplate.opsForHash().get("login_" + username, token);
-            assert userInfoJsonStr != null;
-            UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-            UserContext.setUser(userInfoDTO);
+            String method = httpServletRequest.getMethod();
+            if (!(Objects.equals(requestURI, "/api/shortlink/admin/v1/user") && Objects.equals(method, "POST"))) {
+                String username = httpServletRequest.getHeader("username");
+                String token = httpServletRequest.getHeader("token");
+                if (!StrUtil.isAllNotBlank(username, token)) {
+                    returnJson((HttpServletResponse) servletResponse,  JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                Object userInfoJsonStr;
+                try {
+                    userInfoJsonStr = redisTemplate.opsForHash().get("login_" + username, token);
+                    if (userInfoJsonStr == null) {
+                        returnJson((HttpServletResponse) servletResponse,  JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                        return;
+                    }
+                } catch (Exception exception) {
+                    returnJson((HttpServletResponse) servletResponse,  JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+                    return;
+                }
+                UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
+                UserContext.setUser(userInfoDTO);
+            }
+
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
             UserContext.removeUser();
+        }
+    }
+
+    private void returnJson(HttpServletResponse response, String json) {
+        PrintWriter writer = null;
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try {
+            writer = response.getWriter();
+            writer.print(json);
+
+        } catch (IOException e) {
+        } finally {
+            if (writer != null)
+                writer.close();
         }
     }
 }
